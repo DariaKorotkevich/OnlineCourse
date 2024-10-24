2fb28bcf742e36ae5575e341afcdc913445019db
DariaKorotkevich <120338205+DariaKorotkevich@users.noreply.github.com>
2024-10-24 12:02:47 +0300

    Delete groups directory

--- groups/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from groups.models import GroupOnCourse
-
-
-@admin.register(GroupOnCourse)
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'course')
-    list_editable = ('name', 'course')
