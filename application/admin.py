66de19479e141b2aa53967fd9d7e4a26f22ed300
DariaKorotkevich <120338205+DariaKorotkevich@users.noreply.github.com>
2024-10-24 12:02:08 +0300

    Delete application directory

--- application/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from application.models import Application
-
-
-@admin.register(Application)
-class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'course')
-    list_editable = ('course',)
